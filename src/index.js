56a92c09c6b945743707253dcc3cd8b56bbc61e4
kanedev <kanedev@live.fr>
2019-02-20 11:19:32 +0100

    My first React component with a function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// Creating a new component with a function
+function CoolComponent() {
+    return React.createElement('p', {}, 'Youpi So Cool !')
+  }
+
+  // Linking to the virtual DOM
+ReactDOM.render(
+    React.createElement(CoolComponent),
+    document.getElementById('root')
+  )
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
